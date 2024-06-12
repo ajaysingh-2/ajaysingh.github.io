ec927925bf6477a5a155d048f40e1b68282d246b
Ajay Singh <89483336+ajaysingh-2@users.noreply.github.com>
2024-06-12 11:42:57 +0530

    Module1-solution
    module1-solution

---
+++ app.js
@@ -0,0 +1,37 @@
+// app.js
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.lunchItems = '';
+        $scope.message = '';
+
+        $scope.checkLunch = function () {
+            var items = $scope.lunchItems.split(',');
+            var itemCount = countNonEmptyItems(items);
+
+            if (itemCount === 0) {
+                $scope.message = 'Please enter data first';
+            } else if (itemCount <= 3) {
+                $scope.message = 'Enjoy!';
+            } else {
+                $scope.message = 'Too much!';
+            }
+        };
+
+        function countNonEmptyItems(items) {
+            var count = 0;
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].trim() !== '') {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+})();
